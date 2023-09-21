a31f4d825cb1125853aaab14dc77f78fe6e4629e
Lynzzyr <66968682+Lynzzyr@users.noreply.github.com>
2023-09-20 23:56:44 -0400

    revert to basic drivetrain for reference

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,9 +4,7 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.sensors.BasePigeonSimCollection;
 import com.ctre.phoenix.sensors.CANCoderConfiguration;
-import com.ctre.phoenix.sensors.CANCoderSimCollection;
 import com.ctre.phoenix.sensors.WPI_CANCoder;
 import com.ctre.phoenix.sensors.WPI_Pigeon2;
 import com.revrobotics.CANSparkMax;
@@ -15,18 +13,11 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants.kDrivetrain;
 import frc.robot.Constants.kDrivetrain.kEncoder;
 import frc.robot.Constants.kDrivetrain.kGyroscope;
-import frc.robot.Constants.kDrivetrain.kSimulation;
-import frc.robot.Constants.kDrivetrain.kWheel;
 
 public class Drivetrain extends Subsystem {
 
@@ -47,18 +38,6 @@ public class Drivetrain extends Subsystem {
   // Differential drive & odometry
   private final DifferentialDrive differential;
   private final DifferentialDriveOdometry odometry;
-
-  // Simulation
-  private final CANCoderSimCollection simLeft_enc;
-  private final CANCoderSimCollection simRight_enc;
-
-  private final BasePigeonSimCollection simGyro;
-
-  private final DifferentialDrivetrainSim simDifferential;
-
-  // Shuffleboard & Field2D
-  private final ShuffleboardTab field_tab;
-  private final Field2d field2d;
 
   public Drivetrain() {
 
@@ -114,30 +93,6 @@ public class Drivetrain extends Subsystem {
     differential = new DifferentialDrive(topLeft_mot, topRight_mot);
     odometry = new DifferentialDriveOdometry(getGyroRotation(), getLeftDistanceTraveled(), getRightDistanceTraveled());
 
-    // Simulation
-    simLeft_enc = left_enc.getSimCollection();
-    simRight_enc = right_enc.getSimCollection();
-
-    simGyro = gyro.getSimCollection();
-
-    simDifferential = new DifferentialDrivetrainSim(
-
-      DCMotor.getNEO(2),
-      kSimulation.kGearing,
-      kSimulation.kJKgMeterSq,
-      kSimulation.kMass,
-      kSimulation.kWheelRadius,
-      kSimulation.kTrackWidth,
-      null
-
-    );
-
-    // Shuffleboard & Field2D
-    field_tab = Shuffleboard.getTab("Field2D");
-    field2d = new Field2d();
-
-    field_tab.add("Field", field2d);
-
   }
 
   // Default drive method that acceps trigger inputs
@@ -171,24 +126,9 @@ public class Drivetrain extends Subsystem {
   @Override
   public void periodic() {
 
-    // Odometry & Field2D
+    // Odometry
     odometry.update(getGyroRotation(), getLeftDistanceTraveled(), getRightDistanceTraveled());
-    field2d.setRobotPose(odometry.getPoseMeters());
     
-  }
-
-  @Override
-  public void simulationPeriodic() {
-
-    simDifferential.setInputs(topLeft_mot.get(), -topRight_mot.get());
-    simDifferential.update(0.02);
-
-    simLeft_enc.setRawPosition((int)(simDifferential.getLeftPositionMeters() * (kEncoder.kCountsPerRotation / kWheel.kWheelCircumference)));
-    simLeft_enc.setVelocity((int)((simDifferential.getLeftVelocityMetersPerSecond() * (kEncoder.kCountsPerRotation / kWheel.kWheelCircumference)) / 10));
-    simRight_enc.setRawPosition((int)(simDifferential.getRightPositionMeters() * (kEncoder.kCountsPerRotation / kWheel.kWheelCircumference)));
-    simRight_enc.setVelocity((int)((simDifferential.getRightVelocityMetersPerSecond() * (kEncoder.kCountsPerRotation / kWheel.kWheelCircumference)) / 10));
-    simGyro.setRawHeading(-simDifferential.getHeading().getDegrees());
-
   }
 
 }
